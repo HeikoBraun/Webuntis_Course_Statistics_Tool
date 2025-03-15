afb1d57be93a756e0f82073b3e289e68c85a7cc4
HeikoBraun <github@heiko-braun.org>
2025-03-15 19:48:08 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Webuntis_Course_Statistics_Tool
-Generates statistics of canceled lessons per course from WebUntis
+# Webuntis Course Statistics Tool
+
+This tool allows you to generate statistics about canceled courses from Webuntis data. Webuntis is a popular timetable management software used by schools and universities.
+
+## Prerequisites
+
+Before you can use this tool, make sure you have the following:
+
+- Python 3.x installed on your system.
+- A Webuntis account.
+- Required Python libraries:
+    - webuntis
+    - reportlab
+
+## Installation
+
+1. Clone this repository to your local machine:
+   ```bash
+   git clone https://github.com/yourusername/webuntis-course-statistics.git
+   cd webuntis-course-statistics
+
+2. If you call main.py for the first time, it will generate you a config.toml file in which you have to fill out the data.
---
+++ main.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+import datetime
+import re
+import tomllib
+
+import reportlab
+import reportlab.lib
+import reportlab.lib.colors
+import reportlab.lib.pagesizes
+import reportlab.lib.units
+import reportlab.platypus
+import reportlab.rl_config
+import webuntis
+
+"""https://python-webuntis.readthedocs.io/en/latest/"""
+
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.regular = 0
+        self.irregular = 0
+        self.cancelled = 0
+
+    def __str__(self):
+        self.calc()
+        return f"{self.name}: {self.num}"
+
+    def __add__(self, other):
+        self.regular += other.regular
+        self.irregular += other.irregular
+        self.cancelled += other.cancelled
+        return self
+
+    @property
+    def target(self):
+        return self.regular + self.cancelled
+
+    @property
+    def percent(self):
+        target = self.target
+        if target == 0:
+            return 0
+        return round(100 * (self.regular + self.irregular) / target)
+
+    def incr(self, name=None):
+        if name is None or name == "None":
+            self.regular += 1
+        elif name == "irregular":
+            self.irregular += 1
+        elif name == "cancelled":
+            self.cancelled += 1
+        else:
+            print(f"Error: '{name}' as code is unknown to me.")
+            exit(1)
+
+    def get_table_entry(self):
+        return [
+            self.name,
+            self.regular,
+            self.irregular,
+            self.cancelled,
+            self.target,
+            self.percent,
+        ]
+
+
+def write_initial_toml():
+    with open("config.toml", "w") as fd:
+        fd.write(
+            r"""server="webuntis-server"      # e.g. "herakles.webuntis.com"
+school="webuntis-school-name" # e.g. "FannyLGym"
+username="username"
+password="password"
+# useragent is optional, but should be given with mail for fairness
+# so that an administrator knows whom to contact!
+useragent="WebUntis Test (yourmail@example.com)"
+# classes is optional
+# 1. String => only this class will be used. e.g. "9b"
+# 2. List of strings => these classes will be used. e.g. ["9b", "9d"]
+# 3. not given => all classes matching \d+[a-z] will be used
+classes="9b"
+"""
+        )
+    print(
+        "Wrote out an example configuration file 'config.toml'.\n"
+        "Please fill out the relevant values."
+    )
+
+
+def get_lesson_name(lesson):
+    if lesson.activityType == "Unterricht":
+        if lesson.subjects:
+            ret = str(lesson.subjects[0])
+        else:
+            print(f"Warning! {lesson}")
+            ret = ""
+        if lesson.studentGroup:
+            ret = ret + "/" + str(lesson.studentGroup)
+    else:
+        ret = lesson.lstext
+    return ret
+
+
+def myFirstPage(canvas, doc):
+    canvas.saveState()
+    canvas.setFont("Helvetica-Bold", 16)
+    canvas.drawCentredString(
+        reportlab.rl_config.defaultPageSize[0] / 2.0,
+        reportlab.rl_config.defaultPageSize[1] - 60,
+        doc.title,
+    )
+    canvas.setFont("Helvetica-Bold", 14)
+    canvas.drawCentredString(
+        reportlab.rl_config.defaultPageSize[0] / 2.0,
+        reportlab.rl_config.defaultPageSize[1] - 90,
+        doc.date_period,
+    )
+    canvas.restoreState()
+
+
+def gen_pdf(courses, class_name, school_year):
+    filename = f"Unterrichtsstatistik_für_Klasse_{class_name}.pdf"
+    pdf = reportlab.platypus.SimpleDocTemplate(
+        filename=filename,
+        pagesize=reportlab.lib.pagesizes.A4,
+        title=f"Unterrichtsstatistik für Klasse {class_name}",
+        topMargin=4 * reportlab.lib.units.cm,
+    )
+    pdf.date_period = f"Anfang Schuljahr {school_year} bis {datetime.date.today()}"
+
+    headings = ["Fach", "regulär", "zusätzlich", "ausgefallen", "soll", "Prozent"]
+
+    data = [headings]
+    all_courses = Course("")
+    for course in sorted(courses, key=lambda x: x.name):
+        data.append(course.get_table_entry())
+        all_courses += course
+    data.append(all_courses.get_table_entry())
+
+    table = reportlab.platypus.Table(data=data, repeatRows=4)
+    style = [
+        # header
+        ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
+        ("FONTSIZE", (0, 0), (-1, 0), 12),
+        # data
+        ("FONTNAME", (0, 1), (-1, -1), "Helvetica"),
+        ("FONTSIZE", (0, 1), (-1, -1), 12),
+        ("INNERGRID", (0, 0), (-1, -2), 0.25, reportlab.lib.colors.black),
+        ("BOX", (0, 0), (-1, -2), 0.5, reportlab.lib.colors.black),
+        ("ALIGN", (0, 0), (0, -1), "LEFT"),
+        ("ALIGN", (1, 0), (-1, -1), "RIGHT"),
+        # total (last line)
+        ("INNERGRID", (1, -2), (-1, -1), 0.25, reportlab.lib.colors.black),
+        ("BOX", (1, -1), (-1, -1), 0.5, reportlab.lib.colors.black),
+    ]
+
+    # loop is just to make a grey background on each second line for better readability
+    grey = reportlab.lib.colors.Color(0.8, 0.8, 0.8)
+    for i in range(len(data)):
+        if i % 2 == 1:
+            style.append(("BACKGROUND", (0, i), (-1, i), grey))
+    table.setStyle(reportlab.platypus.TableStyle(style))
+    pdf.build([table], onFirstPage=myFirstPage)
+    print(f"Written {filename}")
+
+
+def work_on(config):
+    with webuntis.Session(
+        username=config["username"],
+        password=config["password"],
+        server=config["server"],
+        school=config["school"],
+        useragent=config.get("useragent", "WebUntis test"),
+    ).login() as session:
+        actual_school_year = session.schoolyears().filter(is_current=True)[0]
+        end_date = actual_school_year.end
+        if end_date > datetime.datetime.now():
+            end_date = datetime.datetime.now()
+        classes = config.get("classes", [])
+        if classes:
+            if isinstance(classes, str):
+                classes = [classes]
+            elif isinstance(classes, list):
+                pass
+            else:
+                print(f"Error: {classes}")
+                exit(1)
+        else:
+            classes = [
+                c.name for c in session.klassen() if re.match(r"\d+[a-z]", c.name)
+            ]
+        for class_name in classes:
+            klasse = session.klassen().filter(name=class_name)[0]
+            lessons = session.timetable_extended(
+                klasse=klasse, start=actual_school_year.start, end=end_date
+            )
+            courses = {}
+            for lesson in lessons:
+                if lesson.activityType != "Unterricht":
+                    continue
+                course_name = get_lesson_name(lesson)
+                if course_name not in courses:
+                    courses[course_name] = Course(course_name)
+                courses[course_name].incr(str(lesson.code))
+            gen_pdf(courses.values(), class_name, actual_school_year.name)
+
+
+if __name__ == "__main__":
+    try:
+        with open("config.toml", "rb") as f:
+            toml_config = tomllib.load(f)
+    except FileNotFoundError:
+        print("Error: configuration file was not found.")
+        write_initial_toml()
+        exit(1)
+    work_on(toml_config)
